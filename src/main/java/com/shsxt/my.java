baa9d9c861cb016a2b27ee14bc554b81d54f1f97
Abadbeginning <2029618609@qq.com>
2017-12-05 10:30:09 +0800

    git idea上传测试

---
+++ src/main/java/com/shsxt/my.java
@@ -0,0 +1,19 @@
+package com.shsxt;
+
+/**
+ * @author Matthew & DEREK & Joseph & Elijah 2283416604@qq.com
+ * @date 04/12/2017 11:18
+ */
+public class my {
+
+    private Integer id;
+    private  String userName;
+
+    public my(Integer id, String userName) {
+        this.id = id;
+        this.userName = userName;
+    }
+
+
+
+}
