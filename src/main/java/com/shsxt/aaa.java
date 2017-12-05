e71a0266e857a82f02b34d0405e1598733d59cc1
Abadbeginning <2029618609@qq.com>
2017-12-05 10:26:51 +0800

    git idea上传测试

---
+++ src/main/java/com/shsxt/aaa.java
@@ -0,0 +1,8 @@
+package com.shsxt;
+
+/**
+ * @author Matthew & DEREK & Joseph & Elijah 2283416604@qq.com
+ * @date 05/12/2017 10:26
+ */
+public class aaa {
+}
---
+++ src/main/java/com/shsxt/constant/CrmConstant.java
@@ -0,0 +1,15 @@
+package com.shsxt.constant;
+
+/**
+ * 
+ *@author lp
+ *  常量类
+ */
+public class CrmConstant {
+	
+	public static final Integer OPS_SUCCESS_CODE=200;
+	public static final String OPS_SUCCESS_MSG="操作成功";
+	
+	public static final Integer OPS_FAILED_CODE=300;
+	public static final String  OPS_FAILED_MSG="操作失败";
+}
